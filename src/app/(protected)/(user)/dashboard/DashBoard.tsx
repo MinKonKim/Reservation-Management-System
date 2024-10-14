3cb27534e9c276250f5a541958db7c50926f8074
alsrhs98 <alsrhs98@gmail.com>
2024-10-14 17:23:34 +0900

    remove: 폴더 구조 뒤엎기

--- src/app/(protected)/(user)/dashboard/DashBoard.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import useAuth from "@/hooks/useAuth";
-
-const DashBoard = () => {
-  const { user } = useAuth();
-  return <div>Welocome {user!.email}</div>;
-};
-
-export default DashBoard;
