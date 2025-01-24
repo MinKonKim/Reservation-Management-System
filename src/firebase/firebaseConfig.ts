feb67e1ecd23a004d252b0626379f7a6811cea19
Minkon Kim <alsrhs98@gmail.com>
2025-01-24 17:00:19 +0900

    feat: firebaseConfig 생성.

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,25 @@
+// Import Firebase modules
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your Firebase configuration object
+const env = process.env;
+const firebaseConfig = {
+  apiKey: env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDER_ID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase app
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Export Firebase services
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
