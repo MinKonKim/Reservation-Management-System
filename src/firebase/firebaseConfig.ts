246a348ead1d141cc0a6461978177e2d78456f3a
Minkon Kim <alsrhs98@gmail.com>
2025-01-24 17:02:57 +0900

    remove: firebaseConfig 에서 storage 삭제제

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -2,7 +2,6 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 // Your Firebase configuration object
 const env = process.env;
@@ -21,5 +20,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 // Export Firebase services
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
-export const storage = getStorage(app);
 export default app;
