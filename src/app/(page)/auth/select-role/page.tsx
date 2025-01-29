bee844263adf6558f0b515e4ecc09fd4b3fcc243
alsrhs98 <alsrhs98@gmail.com>
2025-01-30 00:26:46 +0900

    feat : 어드민/ 유저 회원가입 페이지 분리.

--- src/app/(page)/auth/select-role/page.tsx
+++ src/app/(page)/auth/select-role/page.tsx
@@ -1,5 +1,13 @@
-const page = () => {
-  return <div>page</div>;
+import { Button } from "@/shared/components";
+
+const SelectRolePage = () => {
+  return (
+    <div>
+      <h2 className="title-md">유형 선택</h2>
+      <Button>사용자</Button>
+      <Button>어드민</Button>
+    </div>
+  );
 };
 
-export default page;
+export default SelectRolePage;
--- src/app/(page)/auth/signup/[role]/page.tsx
+++ src/app/(page)/auth/signup/[role]/page.tsx
@@ -1,18 +1,19 @@
-"use client";
-
 import { SignupForm, SignupFormType } from "@/components/signup";
 import { signup } from "@/modules/auth/services/signup";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 import { SubmitHandler } from "react-hook-form";
 
-const SignupPage = () => {
-  const router = useRouter(); // useRouter 훅 사용
-
+const SignupPage = async ({
+  params,
+}: {
+  params: Promise<{ role: string }>;
+}) => {
+  const role = (await params).role;
   const onSubmit: SubmitHandler<SignupFormType> = async (data) => {
     const { email, password } = data;
     const response = await signup(email, password);
     if (response.success) {
-      router.push("/auth/login");
+      redirect("/auth/login");
     } else {
       // TODO:실패 시 사용자에게 알림 (예: 알림 UI 제작)
       alert(response.message);
@@ -23,7 +24,7 @@ const SignupPage = () => {
     <div className="w-full mx-2">
       {/* TODO: 각각의 입력마다 유효성 검사 +  UI  */}
       <h2 className="title-md">회원가입</h2>
-      <SignupForm onSubmit={onSubmit} />
+      {role === "admin" ? null : <SignupForm onSubmit={onSubmit} />}
     </div>
   );
 };
