a61f06dcb5052b1b764d7102dfb1e6b07159aec0
alsrhs98 <alsrhs98@gmail.com>
2025-02-01 01:16:23 +0900

    feat: 유저/어드민 정보 저장 api 구상.

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,6 +1,6 @@
 import { getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -17,4 +17,4 @@ const app =
 
 // Export Firebase services
 export const auth = getAuth(app);
-//const firestore = getFirestore(app);
+export const db = getFirestore(app);
