43628c591a554e680309118c66afa659d2835e91
alsrhs98 <alsrhs98@gmail.com>
2025-02-04 23:21:27 +0900

    add:supabase 설치.

--- src/shared/utils/isFirebaseError.ts
+++
@@ -1,5 +0,0 @@
-import { FirebaseError } from "firebase/app";
-
-export const isFirebaseError = (error: unknown): error is FirebaseError => {
-  return typeof error === "object" && error != null && "code" in error;
-};
