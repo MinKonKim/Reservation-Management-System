32d61352a9e38bbb3ba42ee0c37d31f48be465d9
alsrhs98 <alsrhs98@gmail.com>
2025-02-05 22:31:27 +0900

    remove: 에러나는 부분 제거.

--- src/components/signup/index.ts
+++ src/components/signup/index.ts
@@ -1,3 +1,1 @@
-export { default as AdminInfoForm } from "../infoForm/AdminInfoForm";
-export { default as UserInfoForm } from "../infoForm/UserInfoForm";
 export { default as SignupForm } from "./SignupForm";
