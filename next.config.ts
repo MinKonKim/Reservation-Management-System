d3dbe846b0a1b96aba25627ad65d1f5768c0a0fe
Minkon Kim <alsrhs98@gmail.com>
2025-02-08 17:04:46 +0900

    feat: svgr 설정.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,35 @@
 import type { NextConfig } from "next";
+import { Options } from "./node_modules/deepmerge/index.d";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: { test?: RegExp }) =>
+      rule.test?.test?.(".svg")
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resoureQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: [
+          {
+            loader: "@svgr/webpack",
+            options: {
+              title: true,
+              // svgo: false,
+            },
+          },
+        ],
+      }
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
